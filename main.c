4119b3e5504fd93c5e3f4c0e596fd39285df73fe
Gabriel Ocaga <gocaga@hotmail.com>
2023-01-08 03:40:27 +0000

    feat(file-r+w): add reading from text file

--- main.c
+++ main.c
@@ -6,11 +6,32 @@
 int main()
 {
     char *str;
+    char question[10][256];
+    int answer[10];
+    FILE* fin;
+    int ch = 0;
 
-    char questions[10][256];
+     /* open the file*/
+    fin = fopen("./Questions.txt", "r");
+
+    /* Read out each question, a line at a time and take an answer from the user */
+    for(int i = 0; i < 10; i++ )
+    {
+       fgets(question[i], 256, fin);
+       printf("%s", question[i]);
+        //answer[i] = getchar();
+
+        //prompt the user to type in a character from the console
 
 
-    FILE* fin = fopen("./Questions.txt", "r");
+    }
+
+
+    /* write the contents to a new text file */
+
+
+    /* close when you are done */
+    fclose(fin);
 
 
     return 0;
