1382634711056ed13027caca27d07a3cc4bd694b
Gabriel Ocaga <gocaga@hotmail.com>
2023-01-08 01:50:58 +0000

    feat(file-r+w): delete existing solution
    delete all existing LOC to start from a clean slate.
    old solution causes compiler errors due to incompartible string
    datatypes between a return string from a function and local
    string variable. it requires some sort of string type casting
    which I do not know!

--- main.c
+++ main.c
@@ -3,75 +3,14 @@
 #include <stdint.h>
 #include <string.h>
 
-char *get_string(FILE* stream)
-{
-    int bytes = 0;
-    char *buf = NULL;
-
-
-    char c;
-    while((c = fgetc(stream)) != EOF && c != '\r' && c != '\n')
-    {
-        //We know that we have a valid character
-        bytes++;
-        buf = realloc(buf, bytes+1);
-        if (buf == NULL)
-        {
-            return NULL; //realloc failed
-        }
-        buf[bytes-1] = c;
-
-    }
-
-    if (c == '\r')
-    {
-        c = fgetc(stream);
-        if (c != '\n')
-        {
-            //Oops
-            ungetc(c, stream);
-        }
-    }
-
-    if(buf == NULL)
-    {
-        if (c == EOF)
-        {
-            return NULL;
-        }else{
-            buf = malloc(1);
-        }
-    }
-
-    //null terminate it
-    buf[bytes] = 0;
-
-    return buf;
-}
-
 int main()
 {
     char *str;
 
     char questions[10][256];
 
-    //int linenum = 0;
 
     FILE* fin = fopen("./Questions.txt", "r");
-
-    int i = 0;
-    for(int i = 0; i < 7; i++ )
-    {
-        while(((questions[i] = get_string(fin))!= NULL) && ((questions[i] = get_string(fin))== '\n' ))
-        {
-            //questions[i] = str;
-
-            printf("%s\n", questions[i]);
-            //printf("%d--\t%s\n", linenum++, str);
-            free(questions);
-        }
-    }
-
 
 
     return 0;
