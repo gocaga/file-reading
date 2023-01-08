a01a73f5c836a7a6bcb3fc389a8eebf1c5d05b9c
Gabriel Ocaga <gocaga@hotmail.com>
2023-01-08 12:45:14 +0000

    feat(file-r+w): add receive char input from user

--- main.c
+++ main.c
@@ -10,29 +10,36 @@ int main()
     int answer[10];
     FILE* fin;
     int ch = 0;
+    int x = 0;
 
      /* open the file*/
     fin = fopen("./Questions.txt", "r");
 
     /* Read out each question, a line at a time and take an answer from the user */
-    for(int i = 0; i < 10; i++ )
+    for(int i = 0; i < 8 ; i++ )
     {
-       fgets(question[i], 256, fin);
+       fgets(question[i], 80, fin);
        printf("%s", question[i]);
-        //answer[i] = getchar();
+       while((ch = question[i][x]) == '?');
 
-        //prompt the user to type in a character from the console
-
-
+       /* start prompting the user from line 2 of the text file */
+       if (i > 1)
+       {
+           answer[i] = getchar();   /* prompt the user to type in a character from the console */
+           getchar();              /* expect the Enter key to be pressed */
+       }
     }
 
-
-    /* write the contents to a new text file */
-
-
-    /* close when you are done */
+    /* close when done */
     fclose(fin);
 
+    int z = 0;
+    for (z ; z < 8; z++)
+    {
+       putchar(answer[z]);
+    }
+
+    /* write the contents to a new text file */
 
     return 0;
 }
